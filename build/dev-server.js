d4c4a044cd260071f8fc3f24e0a91e8768562db5
pengzhi <pengzhi@zaizhibo.tv>
2017-02-26 23:24:49 +0800

    first commit

---
+++ build/dev-server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var webpack = require('webpack')
+var config = require('./webpack.config.js')
+
+// 创建一个express实例
+var app = express()
+
+// 调用webpack并把配置传过去
+var compiler = webpack(config)
+
+// 使用webpack-dev-middleware中间件
+var devMiddleware = require('webpack-dev-middleware')(compiler)
+
+// 注册中间件
+app.use(devMiddleware)
+
+app.listen(8888, function(err) {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Listen at http://localhost:8888')
+})
---
+++ build/webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack')
+var path = require('path')
+
+// https://github.com/jantimon/html-webpack-plugin
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/main.js',
+    // entry: {
+    //     'entry.1': '../entry.1.js',
+    //     'entry.2': '../entry.2.js'
+    // },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+      new webpack.BannerPlugin('This file is created by onlyBreak'),
+      new HtmlWebpackPlugin({
+        // filename: path.resolve(__dirname, '../dist/index.html'),
+        template: 'index.html'
+      })
+    ]
+}
