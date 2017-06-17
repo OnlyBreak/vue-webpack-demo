2fcc652a8749168b8b1fb7af850e8c5d1cb54321
pengzhi <pengzhi@zaizhibo.tv>
2017-06-17 13:38:15 +0800

    configure proxy middleware

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,12 +1,19 @@
 var express = require('express')
 var webpack = require('webpack')
-var config = require('./webpack.dev.conf.js')
+var config = require('../config')
+var webpackConfig = require('./webpack.dev.conf.js')
+
+// Define HTTP proxies to your custom API backend
+// https://github.com/chimurai/http-proxy-middleware
+var proxyTable = config.dev.proxyTable
+
+var proxyMiddleware = require('http-proxy-middleware')
 
 // 创建一个express实例
 var app = express()
 
 // 调用webpack并把配置传过去
-var compiler = webpack(config)
+var compiler = webpack(webpackConfig)
 
 // 使用webpack-dev-middleware中间件
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
@@ -14,6 +21,17 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
         colors: true,
         chunks: false
     }
+})
+
+// proxy api requests
+Object.keys(proxyTable).forEach(function (context) {
+  var options = proxyTable[context]
+
+  if (typeof options === 'string') {
+    options = { target: options }
+  }
+
+  app.use(proxyMiddleware(options.filter || context, options))
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler)
--- config/index.js
+++ config/index.js
@@ -5,5 +5,18 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist/'),
     assetsSubDirectory: 'static'
+  },
+  dev: {
+    env: '"development"',
+    proxyTable: {
+      '/auth': {
+        target: 'http://localhost:8889',
+        changeOrigin: true
+      },
+      '/api': {
+        target: 'http://localhost:8889',
+        changeOrigin: true
+      }
+    }
   }
 }
